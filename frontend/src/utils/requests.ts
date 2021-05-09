4bb6c002859b838485239c70547ec8e9fb6fab8c
FelipeGSF <felipegasques@hotmail.com>
2021-05-09 17:41:32 -0400

    Corrigindo o requests.ts

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'; // Definindo que a BASE_URL da aplicação vai ser para pegar a var de ambiente do netlify
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://sds3-felipegsf.herokuapp.com'; // Definindo que a BASE_URL da aplicação vai ser para pegar a var de ambiente do netlify
+
+//se a variável de ambiente nao estiver definida "REACT_APP_BACKEND_URL" que é o caso quando rodamos no computador local o BASE_URL vai pegar por padrão o "htpp://localhost:8080"
